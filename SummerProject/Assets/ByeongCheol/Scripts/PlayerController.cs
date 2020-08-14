ba87d27ddd9bb3cc7cef3af3f804344201c4595e
ByeongCheolJo <zxas1245@naver.com>
2020-08-15 04:20:59 +0900

    # 5. 플레이어 좌우 동시입력 에러 확인, Rigidbody velocity 이용한 플레이어 이동 스크립트 작성중
    # 세부사항
    1. 한쪽 방향키 입력중 반대 방향키 입력 시 나중에 입력된 값을 인식하도록 만들었으나 좌-우, 우-좌 둘 중 하나만 수행가능해서 수정중
    2. FixedUpdate 에서 rigidbody velocity 를 이용한 플레이어 이동 스크립트 공부중.

--- SummerProject/Assets/ByeongCheol/Scripts/PlayerController.cs
+++ SummerProject/Assets/ByeongCheol/Scripts/PlayerController.cs
@@ -20,22 +20,15 @@ public class PlayerController : MonoBehaviour
 
     Animator anim;
 
-
-    private Coroutine inputKeyRoutine;
-    private float inputLate = 0.03f;
+    Rigidbody rigid;
 
     void Start()
     {
         StartCoroutine(this.GameSpeedController());
-
-        //좌우 동시입력문제 해결 - 사용x 2020.8.14
-        //StartCoroutine(this.SimultaniousInput());
-    }
-
-    void Awake()
-    {
         anim = GetComponentInChildren<Animator>();
+
     }
+
 
     // Player가 오브젝트와 충돌 시 에니메이터 종료
     private void OnCollisionEnter(Collision collision)
@@ -59,18 +52,19 @@ public class PlayerController : MonoBehaviour
     }
 
 
-
-
     void Update()
     {
         // 전진 키 입력 속도증가
         forwardSpeed += Input.GetAxisRaw("Vertical") * 0.5f;
 
-
+        Debug.Log(Input.inputString);
         //Debug.Log("GetAxisRaw(Horizontal) : "+Input.GetAxisRaw("Horizontal"));
         //Debug.Log("get key D : "+Input.GetKey(KeyCode.D));
 
-        //한쪽 횡방향 입력중 다른방향 입력시 방향전환 및 횡방향 방향전환
+        //한쪽 횡방향 입력중 다른방향 입력시 방향전환 및 횡방향 
+
+        //오른쪽-왼쪽 은 되지만 왼쪽-오른쪽도 가능하게 코드를 짜야함 - 수정 요망
+
         if (Input.GetKey(KeyCode.D) && Input.GetAxisRaw("Horizontal") == 0)
         {
             Debug.Log("오른쪽 진행 도중 왼쪽진행");
@@ -90,113 +84,12 @@ public class PlayerController : MonoBehaviour
         hCurrentSpeed = IncrementSide(hCurrentSpeed, hTargetSpeed, acceleration);
 
 
-        // 좌우 입력이 들어오면 월드 기준 좌우 이동 아니면 셀프.. 수정요망.. 
         transform.Translate(hCurrentSpeed * Time.deltaTime, 0f, forwardSpeed * Time.deltaTime, Space.World);
-        //transform.localPosition = Vector3.Lerp(transform.position, new Vector3(hCurrentSpeed * Time.deltaTime, 0f, forwardSpeed + Time.deltaTime), Time.deltaTime);
 
         // 좌,우 이동 버튼 입력 시 애니매이션 작동
         anim.SetBool("isRight", hCurrentSpeed > 0);
         anim.SetBool("isLeft", hCurrentSpeed < 0);
-
     }
-
-
-
-
-    // 좌(우) 키 입력 도중 우(좌)입력 시 입력 값이 0(좌, 우 이동 둘다 안됨)을 하지않게 하는 함수
-    /*
-    private IEnumerator SimultaniousInput()
-    {
-        while (true)
-        {
-            switch (Input.inputString)
-            {
-                //case "AD":
-                //case "ad":
-                //    //Debug.Log("AD");
-                //    break;
-
-
-                case "A":
-                case "a":
-                    if (this.inputKeyRoutine == null)
-                    {
-                        //Debug.Log("A 입력 함수 실행");
-                        this.inputKeyRoutine = StartCoroutine(this.InputA());
-                    }
-                    break;
-
-                case "D":
-                case "d":
-                    if (this.inputKeyRoutine == null)
-                    {
-                        //Debug.Log("D 입력 함수 실행");
-                        this.inputKeyRoutine = StartCoroutine(this.InputD());
-                    }
-                    break;
-
-
-
-            }
-            yield return null;
-        }
-    }
-    private IEnumerator InputA()
-    {
-        float i = this.inputLate;
-        bool d = false;
-        while (i > 0)
-        {
-            if (Input.inputString == "D" || Input.inputString == "d")
-            {
-                //Debug.Log("추가 입력!");
-                d = true;
-            }
-
-            i -= Time.deltaTime;
-            yield return null;
-        }
-
-        if(d == false)
-        {
-            //Debug.Log("A");
-            isLeft = true;
-            isRight = false;
-        }
-        this.inputKeyRoutine = null;
-    }
-    private IEnumerator InputD()
-    {
-        float i = this.inputLate;
-        bool a = false;
-        while (i > 0)
-        {
-            if (Input.inputString == "A" || Input.inputString == "a")
-            {
-                //Debug.Log("추가 입력!");
-                a = true;
-            }
-
-            i -= Time.deltaTime;
-            yield return null;
-        }
-
-        //if (a == true)
-        //{
-        //    //Debug.Log("D+A");
-        //}
-        //else
-        if(a==false)
-        {
-            //Debug.Log("D");
-            isRight = true;
-            isLeft = false;
-
-        }
-        this.inputKeyRoutine = null;
-    }
-    */
-
 
     // 좌우 이동 시 가속도 적용 함수
     private float IncrementSide(float n, float target, float a)
---
+++ SummerProject/Assets/ByeongCheol/Scripts/PlayerController2.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+
+    Animator anim;
+    Rigidbody rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        // 충돌 발생 시 애니매이션 비활성화 및 쿼터뷰 카메라 정지
+        anim.enabled = false;
+        Follow vari = GameObject.Find("Main Camera").GetComponent<Follow>();
+        vari.target = null;
+
+        Debug.Log("Game Over");
+
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        gameObject.GetComponent<PlayerController2>().enabled = false;
+    }
+
+
+    //물리적 이동 관련은 FixedUpdate부분에 짜는게 좋다하여 하는중
+    void FixedUpdate()
+    {
+        Vector3 vec = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        rigid.AddForce(vec, ForceMode.Impulse);
+        Debug.Log(rigid.velocity);
+
+
+    }
+}
