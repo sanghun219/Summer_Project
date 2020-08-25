35d7dfcac458214d7072d54037a1de70762c75bb
sanghun219 <sanghun219@naver.com>
2020-08-26 00:01:24 +0900

    머지 완료!

--- SummerProject/Assets/ByeongCheol/Scripts/InGameUIManager.cs
+++ SummerProject/Assets/ByeongCheol/Scripts/InGameUIManager.cs
@@ -4,28 +4,29 @@ using UnityEngine;
 
 public class InGameUIManager : MonoBehaviour
 {
-
     public GameObject InGameUI;
 
+    private Player player;
 
-    void OnEnable()
+    private void OnEnable()
     {
         Debug.Log("인에이블");
-        Player.GameOverEvent += this.GameOverEvent;
-    }
-    void OnDisable()
-    {
-        Debug.Log("디스에이블");
-        Player.GameOverEvent -= this.GameOverEvent;
+        player.GameOverEvent += this.GameOverEvent;
     }
 
-    void GameOverEvent()
+    private void OnDisable()
+    {
+        Debug.Log("디스에이블");
+        player.GameOverEvent -= this.GameOverEvent;
+    }
+
+    private void GameOverEvent()
     {
         Debug.Log("게임오버 함수 실행");
         if (this.InGameUI.activeSelf == true)
         {
             //this.InGameUI.SetActive(false);
-            if(GameObject.Find("UI").transform.Find("GameOverUI"))
+            if (GameObject.Find("UI").transform.Find("GameOverUI"))
             {
                 Debug.Log("게임오버 UI 찾기 성공");
                 GameObject.Find("UI").transform.Find("GameOverUI").gameObject.SetActive(true);
@@ -35,7 +36,5 @@ public class InGameUIManager : MonoBehaviour
                 Debug.Log("게임오버 UI 찾기 실패");
             }
         }
-
     }
-
 }
