858c47217a14bdd6027cd5666d2da43fbd005015
ByeongCheolJo <zxas1245@naver.com>
2020-09-04 21:38:41 +0900

    # sound UI, 스크립트
    # 세부사항
    1. sound on/off  토글 UI 생성 및 스크립트 작성
      - 사운드 소스 찾는중..

---
+++ SummerProject/Assets/ByeongCheol/Scripts/ToggleManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleManager : MonoBehaviour
+{
+    public Toggle toggle;
+    void Start()
+    {
+
+        toggle.onValueChanged.AddListener((value) =>
+     {
+         MyListener(value);
+     });
+        //Do this in Start() for example 
+    }
+
+    public void MyListener(bool value)
+    {
+        if (value)
+        {
+            GameObject.FindWithTag("SoundManager").GetComponent<SoundManager>().enabled = true;
+            //do the stuff when the toggle is on 
+        }
+        else
+        {
+            GameObject.FindWithTag("SoundManager").GetComponent<SoundManager>().enabled = false;
+            //do the stuff when the toggle is off 
+        }
+
+    }
+}
